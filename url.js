394bd30b412fc006a17d7b69821304812097f22e
Tai-shang-chun <71634883+Tai-shang-chun@users.noreply.github.com>
2022-11-07 00:26:59 +0800

    Add files via upload

---
+++ url.js
@@ -0,0 +1,15 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 除最後一項外，其餘項目用逗號分隔
+ * 2. 網址請用引號包起來
+ */
+
+
+const urls = [
+	'https://forms.gle/PhwdSguq1Xeixg5A6',
+	'https://forms.gle/FtZwrCaMRKS6rcJx5',
+	'https://forms.gle/uaWvtzhnebjE5wbU7',
+	'https://forms.gle/KgJacsxWTHeciSxg6'
+];
